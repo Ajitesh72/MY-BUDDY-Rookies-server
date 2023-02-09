f48e7a81de84ee0769d93e2ea8f7e10d5d9004cb
Ajitesh <Ajitesh72>
2023-02-10 00:21:02 +0530

    added nodemailer and stripe

--- server.js
+++ server.js
@@ -1,29 +1,28 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const authenticateJWT = require("./middleware/authJwt.js")
-const dotenv = require("dotenv")
+const authenticateJWT = require("./middleware/authJwt.js");
+const dotenv = require("dotenv");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const UserModel = require("./model/userModel");
-dotenv.config()
+const secretKey =
+  "sk_test_51MHWQrSA2zGpkJ04CaLFhfjwCp89K9iBaWUYtoUGRwbJSOWyo7CjRLQEilZ0VLVTxiEeFk4VykQITSoRfDSYVezF006bbYCgVT"; //secret key for stripe.js
+const stripe = require("stripe")(secretKey);
+const nodemailer = require("nodemailer");
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-const port = 1337 || process.env.PORT
-
-
+const port = 1337 || process.env.PORT;
 
 mongoose
   .connect(
     `mongodb+srv://${process.env.MongoUsername}:${process.env.MongoPassword}@cluster0.35vxia3.mongodb.net/Users?retryWrites=true&w=majority`
   )
   .then(console.log("DB CONNECTED"));
-
-
 
 app.post("/api/register", async (req, res) => {
   console.log(req.body);
@@ -73,36 +72,96 @@ app.post("/api/login", async (req, res) => {
   }
 });
 
+app.use(authenticateJWT);
 
-app.use(authenticateJWT)
+//get and post request by mehdi   and stripe.js premium
 
-//get and post request by mehdi
-
-
-app.post("/api/mehdi" ,async (req,res)=>{
+app.post("/api/mehdi", async (req, res) => {
   console.log(req.body);
   res.json({
-    information : "ok i got ur name",
-  })
-})
-app.get("/api/mehdigetData" ,async (req,res)=>{
+    information: "ok i got ur name",
+  });
+});
+app.get("/api/mehdigetData", async (req, res) => {
   console.log(req.body);
   res.json({
     // information : "ok i got ur name",
-    userData: req.user
-  })
-})
+    userData: req.user,
+  });
+});
+app.post("/create-checkout-session", async (req, res) => {
+  console.log(req.user);
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    customer_email: req.user.email,
+    line_items: [
+      {
+        price_data: {
+          currency: "inr",
+          product_data: {
+            name: "PREMIUM MEMBERSHIP",
+          },
+          unit_amount: 20000,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: "http://localhost:3000/stripepaymentsuccess",
+    // success_url: "http://localhost:3000/order/success?session_id={CHECKOUT_SESSION_ID}",
+    cancel_url: "http://localhost:3000/stripepaymentcancel",
+  });
+
+  res.json({ id: session.id });
+});
+
+app.post("/sendEmail", async (req, res) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "dubeyajitesh07@gmail.com",
+      pass: "ostyygulkadbjicg",
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  const mailOptions = {
+    from: "dubeyajitesh07@gmail.com",
+    to: req.user.email,
+    subject: "CONFIRMATION MAIL FOR SCHEDULED APPOINTMENT",
+    text: "Thank you for choosing us",
+    html: '<b>THANK YOU FOR TRUSTING US</b>,<br/><hr/> <img src="cid:unique@kreata.ee"/> <br/><p>REPLY TO THIS MESSAGE FOR ANY QUERIES.</p><p>We will try to reach out to you as soon as possible</p>',
+    attachments: [
+      {
+        filename: "thankyou.jpeg",
+        path: "./thankyou.jpeg",
+        cid: "unique@kreata.ee", //same cid value as in the html img src
+      },
+    ],
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      res.json({ status: "notok" });
+    } else {
+      console.log("Email sent: " + info.response);
+      res.json({ status: "ok" });
+    }
+  });
+});
 
 // MAIN CODE
+// const secretKey = "sk_test_secret_api_key";    //secret key for stripe.js jo top mei likha hu
 
-app.get("/api/home/userData" ,async (req,res)=>{
+app.get("/api/home/userData", async (req, res) => {
   res.json({
-    userData: req.user
-  })
-})
-
+    userData: req.user,
+  });
+});
 
 app.listen(1337, () => {
   console.log(`\x1b[33m   Server started on ${port}  \x1b[0m`);
-  
 });
